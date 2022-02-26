91cb1221d87c9bde6ed79aa6a0164fcf3f3e1ea6
Ashish-Abraham <ashishabraham22@gmail.com>
2022-02-26 15:28:55 +0530

    added api files

---
+++ api/main.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from flask import Flask, render_template, url_for, request, redirect, jsonify
+from PIL import Image
+import numpy as np
+import os
+from torch.functional import split
+from ml_predict import predict_dog
+import base64
+from decouple import config
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def predict():
+    key_dict = request.get_json()
+    image = key_dict["image"]
+    imgdata = base64.b64decode(image)
+    dog = predict_vehicle(imgdata)
+    response = {
+        "result": dog,
+    }
+    response = jsonify(response)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
---
+++ api/test.py
@@ -0,0 +1,5 @@
+import requests
+
+response = requests.post("")
+
+print(resp.text)
